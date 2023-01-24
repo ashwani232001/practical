4ed248446e42b95088de1cd9e52e774bcb1793eb
ashwani232001 <122799822+ashwani232001@users.noreply.github.com>
2023-01-25 02:31:47 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,20 @@
+class Student1{
+int slno;
+String name;
+}
+class Student{
+public static void main(String...args){
+Student1 s=new Student1();
+s.slno=71;
+s.name="ASHWANI";
+
+System.out.println(s.slno+" "+s.name);
+
+s.slno=71;
+s.name="jai";
+
+System.out.println(s.slno+" "+s.name);
+
+
+}
+}

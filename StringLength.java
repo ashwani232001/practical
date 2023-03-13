cc6857e0a9bbb7bbbffaeb2e2aca2c48df6eca8e
ashwani232001 <122799822+ashwani232001@users.noreply.github.com>
2023-03-13 17:24:36 +0530

    Create StringLength.java

---
+++ StringLength.java
@@ -0,0 +1,14 @@
+package String;
+
+public class StringLength {
+    public static void main(String[] args) {
+        String str = "Anudip Foundation";
+        
+         // One way to find string length.
+        System.out.println("The string length of '"+str+"' is : "+str.length());
+        
+        // Another way to find string length.
+        int len = str.length();
+        System.out.println("The string length of '"+str+"' is : "+len);
+    }
+}

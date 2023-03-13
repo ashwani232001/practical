57f46382f270fe38562adabe84bc5d935e560d9e
ashwani232001 <122799822+ashwani232001@users.noreply.github.com>
2023-03-13 16:21:28 +0530

    Create ImmutableTest.java

---
+++ ImmutableTest.java
@@ -0,0 +1,14 @@
+package String;
+
+public class ImmutableTest {
+    public static void main(String[] args) {
+
+//        String a = "Ashwani ";                             //it's connot concatinate
+//        a.concat("Singh");
+        
+        
+        String a = "Ashwani ";
+        a = a.concat("Singh");                               //it's  concatinate
+        System.out.println(a);
+    }
+}
